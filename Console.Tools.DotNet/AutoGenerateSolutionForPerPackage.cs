6ef9ed11dd6c1e1f34a280e822dcfbce00ad5e5a
nickniverson <nlniverson@gmail.com>
2023-09-29 07:54:38 -0400

    - add unit test projects to generated solution - refactor project reference to use a class instead of just strings - simplify usages of project directory

--- Console.Tools.DotNet/AutoGenerateSolutionForPerPackage.cs
+++ Console.Tools.DotNet/AutoGenerateSolutionForPerPackage.cs
@@ -6,21 +6,40 @@ using Spectre.Console.Cli;
 
 namespace Console.Tools.DotNet
 {
+	// # REQUIREMENTS
+	// should implement 2 modes:
+	//	- auto
+	//		- should automatically determine project types (this is how it is currently implemented)
+	//	- explicit config
+	//		- should accept an explicit list of projects that we want to build
+	//		  solutions for (in the event that the auto-logic is faulty in some
+	//		  way or if a new project type is added that hasn't been added to
+	//		  this tool just yet)
+	//  - should have option to put the new solutions at the repo root or alongside the project for which it was created
+	//		- default should probably be next to the rest of the solutions or perhaps a configurable location?
+	//		- I want it to be automatic rather than require explicit configuration... and having them closer to the root makes them more discoverable... which I prefer... (not sure of any downsides that I can think of)
+
 	public enum ProjectType
 	{
 		Unknown,
 		WebApi,
 		ConsoleApp,
-		AzureFunctionApp
+		AzureFunctionApp,
+		TestProject,
 	}
+
 
 	public class ProjectMetadata
 	{
 		public string FullPath { get; private set; }
 
+		public string Directory { get; private set; }
+
 		public ProjectType Type { get; set; }
 
-		public List<string> ProjectReferences { get; private set; } = new List<string>();
+		public List<PackageReference> PackageReferences { get; private set; } = new List<PackageReference>();
+
+		public List<ProjectReference> ProjectReferences { get; private set; } = new List<ProjectReference>();
 
 		public string? Sdk { get; set; }
 
@@ -36,7 +55,31 @@ namespace Console.Tools.DotNet
 			}
 
 			FullPath = fullPath;
+
+			Directory = Path.GetDirectoryName(fullPath)!;
 		}
+	}
+
+
+	public class PackageReference
+	{
+		public PackageReference(string include)
+		{
+			Include = include ?? throw new ArgumentNullException(nameof(include));
+		}
+
+		public string Include { get; set; }
+	}
+
+
+	public class ProjectReference
+	{
+		public ProjectReference(string include)
+		{
+			Include = include ?? throw new ArgumentNullException(nameof(include));
+		}
+
+		public string Include { get; set; }
 	}
 
 
@@ -82,11 +125,23 @@ namespace Console.Tools.DotNet
 	}
 
 
+	public class TestProjectSpecification : IProjectTypeSpecification
+	{
+		public ProjectType Type => ProjectType.TestProject;
+
+		public bool IsMatch(ProjectMetadata projectMetadata)
+		{
+			return projectMetadata.PackageReferences.Any(x => x.Include == "Microsoft.NET.Test.Sdk");
+		}
+	}
+
+
 
 	public interface IProjectReader
 	{
-		ProjectMetadata ReadProject(string path);
-		IEnumerable<ProjectMetadata> ReadProjects(string path);
+		ProjectMetadata ReadProject(string projectFilePath);
+
+		IEnumerable<ProjectMetadata> ReadProjects(string directoryPath);
 	}
 
 
@@ -100,29 +155,44 @@ namespace Console.Tools.DotNet
 			_projectTypeSpecifications = projectTypeSpecifications ?? throw new ArgumentNullException(nameof(projectTypeSpecifications));
 		}
 
-		public ProjectMetadata ReadProject(string path)
+		public ProjectMetadata ReadProject(string projectFilePath)
 		{
-			var doc = XDocument.Load(path);
-			var projectMetadata = new ProjectMetadata(path);
+			if (string.IsNullOrWhiteSpace(projectFilePath))
+			{
+				throw new ArgumentException($"'{nameof(projectFilePath)}' cannot be null or whitespace.", nameof(projectFilePath));
+			}
+
+			var doc = XDocument.Load(projectFilePath);
+			var projectMetadata = new ProjectMetadata(projectFilePath);
 
 			projectMetadata.Sdk = doc.Descendants("Project").FirstOrDefault()?.Attribute("Sdk")?.Value;
 			projectMetadata.OutputType = doc.Descendants("OutputType").FirstOrDefault()?.Value;
 			projectMetadata.AzureFunctionsVersion = doc.Descendants("AzureFunctionsVersion").FirstOrDefault()?.Value;
+
+			projectMetadata.PackageReferences.AddRange(
+				doc
+				.Descendants("PackageReference")
+				.Select(packageReference => new PackageReference(
+					packageReference.Attribute("Include")!.Value))
+				.ToList());
+
 			projectMetadata.ProjectReferences.AddRange(
 				doc
 				.Descendants("ProjectReference")
 				.Select(projectReference =>
-					Path.GetFullPath(
-						Path.Combine(
-							Path.GetDirectoryName(path),
-							projectReference.Attribute("Include").Value)
-					))
+					new ProjectReference(
+						include: Path.GetFullPath(
+							Path.Combine(
+								Path.GetDirectoryName(projectFilePath)!,
+								projectReference.Attribute("Include")!.Value)
+						)))
 				.ToList());
 
 			SetProjectType(projectMetadata);
 
 			return projectMetadata;
 		}
+
 
 		public IEnumerable<ProjectMetadata> ReadProjects(string path)
 		{
@@ -141,6 +211,7 @@ namespace Console.Tools.DotNet
 			}
 		}
 
+
 		private void SetProjectType(ProjectMetadata projectMetadata)
 		{
 			foreach (var projectType in _projectTypeSpecifications)
@@ -157,27 +228,48 @@ namespace Console.Tools.DotNet
 		}
 	}
 
+
 	public interface ISolutionCreator
 	{
-		void GenerateSolution(ProjectMetadata project);
+		void GenerateSolution(ProjectMetadata project, string rootDirectory);
 	}
+
 
 	public class SolutionCreator : ISolutionCreator
 	{
 		private readonly IProjectReader _projectReader;
-		private readonly HashSet<string> _addedProjects = new HashSet<string>(); // Added to track projects already in the solution
+		private readonly HashSet<string> _addedProjects = new(); // Added to track projects already in the solution
+		private readonly List<ProjectMetadata> _allProjects = new();
+
 
 		public SolutionCreator(IProjectReader projectReader)
 		{
 			_projectReader = projectReader ?? throw new ArgumentNullException(nameof(projectReader));
 		}
 
-		public void GenerateSolution(ProjectMetadata project)
-		{
-			_addedProjects.Clear();
 
-			var solutionFileName = $"{Path.GetFileNameWithoutExtension(project.FullPath)}.CI.sln";
-			var solutionFilePath = Path.Combine(Path.GetDirectoryName(project.FullPath), solutionFileName);
+		public void GenerateSolution(ProjectMetadata project, string rootDirectory)
+		{
+			if (project is null)
+			{
+				throw new ArgumentNullException(nameof(project));
+			}
+
+			if (string.IsNullOrEmpty(rootDirectory))
+			{
+				throw new ArgumentException($"'{nameof(rootDirectory)}' cannot be null or empty.", nameof(rootDirectory));
+			}
+
+			_addedProjects.Clear();
+			_allProjects.Clear();
+
+			// load all projects once
+			_allProjects
+				.AddRange(_projectReader
+				.ReadProjects(rootDirectory));
+
+			var solutionFileName = $"{Path.GetFileNameWithoutExtension(project.FullPath)}.CI.generated.sln";
+			var solutionFilePath = Path.Combine(project.Directory, solutionFileName);
 
 			// Delete the solution file if it already exists
 			if (File.Exists(solutionFilePath))
@@ -185,12 +277,25 @@ namespace Console.Tools.DotNet
 				File.Delete(solutionFilePath);
 			}
 
+			LogInfo(project, solutionFileName, solutionFilePath);
+
 			// Create a new solution file
-			Dotnet($"new sln -n {Path.GetFileNameWithoutExtension(solutionFileName)} -o {Path.GetDirectoryName(project.FullPath)}");
+			Dotnet($"new sln -n {Path.GetFileNameWithoutExtension(solutionFileName)} -o {project.Directory}");
 
 			// Add the projects to the solution recursively
 			AddProjectToSolution(solutionFilePath, project);
 		}
+
+
+		private void LogInfo(ProjectMetadata project, string solutionFileName, string solutionFilePath)
+		{
+			AnsiConsole.MarkupLine($"[green]creating solution:[/] '{solutionFileName}'");
+			AnsiConsole.MarkupLine($"	[green]for project type:[/] {project.Type}");
+			AnsiConsole.MarkupLine($"	[green]for project path:[/] {project.FullPath}");
+			AnsiConsole.MarkupLine($"	[green]at:[/] {solutionFilePath}");
+			AnsiConsole.WriteLine();
+		}
+
 
 		private void AddProjectToSolution(string solutionFilePath, ProjectMetadata project)
 		{
@@ -205,17 +310,47 @@ namespace Console.Tools.DotNet
 
 			_addedProjects.Add(project.FullPath);  // Mark this project as added
 
-			// Add the referenced projects to the solution
+			// Recursively add the referenced projects to the solution
+			RecursivelyAddReferencedProjectsToSolution(solutionFilePath, project);
+
+			// Recursively add test projects to the solution
+			RecursivelyAddTestProjectsToSolution(solutionFilePath, project);
+		}
+
+
+		private void RecursivelyAddReferencedProjectsToSolution(string solutionFilePath, ProjectMetadata project)
+		{
 			foreach (var referencePath in project.ProjectReferences)
 			{
-				var referenceProject = _projectReader.ReadProject(referencePath);
+				var referenceProject = _allProjects.FirstOrDefault(x => x.FullPath == referencePath.Include);
 
-				if (referenceProject != null)
+				if (referenceProject == null)
 				{
-					AddProjectToSolution(solutionFilePath, referenceProject);
+					continue;
 				}
+
+				AddProjectToSolution(solutionFilePath, referenceProject);
 			}
 		}
+
+
+		private void RecursivelyAddTestProjectsToSolution(string solutionFilePath, ProjectMetadata project)
+		{
+			IEnumerable<ProjectMetadata> testProjects = _allProjects.Where(project => project.Type == ProjectType.TestProject);
+
+			foreach (var testProject in testProjects)
+			{
+				// if no references to the current project are found, continue
+				if (!testProject.ProjectReferences.Any(projectReference => projectReference.Include == project.FullPath))
+				{
+					continue;
+				}
+
+				// if test project references current project, add it to the solution
+				AddProjectToSolution(solutionFilePath, testProject);
+			}
+		}
+
 
 		private void Dotnet(string arguments)
 		{
@@ -259,19 +394,16 @@ namespace Console.Tools.DotNet
 
 		public override int Execute([NotNull] CommandContext context, [NotNull] AutoGeneratePerPackageSettings settings)
 		{
-			List<ProjectMetadata> projects = _projectReader
+			List<ProjectMetadata> allProjects = _projectReader
 				.ReadProjects(settings.Path)
 				.ToList();
 
-			projects
-				.Where(project => project.Type != ProjectType.Unknown)
+			allProjects
+				.Where(project => project.Type != ProjectType.Unknown && project.Type != ProjectType.TestProject)
 				.ToList()
 				.ForEach(project =>
 				{
-					AnsiConsole.MarkupLine($"[green]creating solution for project type:[/]  '{project.Type}' [green]with path[/] '{project.FullPath}'");
-					AnsiConsole.WriteLine();
-
-					_solutionCreator.GenerateSolution(project);
+					_solutionCreator.GenerateSolution(project, rootDirectory: settings.Path);
 				});
 
 			return 0;
